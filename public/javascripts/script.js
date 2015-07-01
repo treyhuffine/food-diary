4ba02df5da26190bf50482e8fb2419f260c2edeb
Trey Huffine <treyhuffine@gmail.com>
2015-06-30 21:59:18 -0700

    Food input models

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,10 +2,16 @@ var app = angular.module("foodDiary", []);
 
 app.controller("DiaryCtlr", function($scope, $http) {
   $scope.userList = [];
+  $scope.foodList =[];
   $scope.user = {};
 
   $scope.saveUser = function() {
     $scope.currentUser = $scope.user;
     $scope.userList.push($scope.user);
+  };
+  $scope.addFood = function() {
+    $scope.food.date = new Date();
+    $scope.foodList.push($scope.food);
+    $scope.food = {};
   };
 });
