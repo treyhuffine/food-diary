2006c2a09d714232305ba069168b3ca8d8076804
Trey Huffine <treyhuffine@gmail.com>
2015-06-30 23:28:29 -0700

    Add BMI image

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,6 +4,7 @@ app.controller("DiaryCtlr", function($scope, $http) {
   $scope.userList = [];
   $scope.foodList =[];
   $scope.user = {};
+  $calsPerPound = 3500;
 
   $scope.saveUser = function() {
     $scope.currentUser = $scope.user;
