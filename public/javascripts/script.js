35944cfb23254f15e9f8add7956c9590d9f9f64c
Trey Huffine <treyhuffine@gmail.com>
2015-06-30 21:40:44 -0700

    Move user statistics

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,11 @@
 var app = angular.module("foodDiary", []);
 
 app.controller("DiaryCtlr", function($scope, $http) {
+  $scope.userList = [];
   $scope.user = {};
+
+  $scope.saveUser = function() {
+    $scope.currentUser = $scope.user;
+    $scope.userList.push($scope.user);
+  };
 });
